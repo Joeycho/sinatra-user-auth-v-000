27f2dd2087b69ea4a93d52a634027e7215a2443b
Joey Cho <cjy12182000@naver.com>
2019-01-26 05:52:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,11 +29,11 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    puts @user
-    session[:id] = @user.id
     if@user == nil
       redirect 'sessions/login'
     end
+    session[:id] = @user.id
+   
     redirect '/users/home'
   end
 
